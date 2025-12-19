937543a816e313820d483d8a43d4a6c115e424c6
Elyas Alemi <elyasalemi10@gmail.com>
2025-12-19 11:02:39 +1100

    Make remove-attendees migration safer with existence check

--- supabase-remove-attendees.sql
+++ supabase-remove-attendees.sql
@@ -1,6 +1,15 @@
 -- Remove attendees and email_attendees columns from events table
 -- These fields are no longer needed
 
-ALTER TABLE events DROP COLUMN IF EXISTS attendees;
-ALTER TABLE events DROP COLUMN IF EXISTS email_attendees;
+-- Only run if events table exists
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'events') THEN
+    ALTER TABLE events DROP COLUMN IF EXISTS attendees;
+    ALTER TABLE events DROP COLUMN IF EXISTS email_attendees;
+    RAISE NOTICE 'Removed attendees columns from events table';
+  ELSE
+    RAISE NOTICE 'Events table does not exist - skipping migration';
+  END IF;
+END $$;
 
