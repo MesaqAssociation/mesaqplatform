2c12f92a4c392e41680ff017c44c3328b902d338
Elyas Alemi <elyasalemi10@gmail.com>
2025-12-19 11:04:38 +1100

    Revert to simple remove-attendees migration

--- supabase-remove-attendees.sql
+++ supabase-remove-attendees.sql
@@ -1,15 +1,6 @@
 -- Remove attendees and email_attendees columns from events table
 -- These fields are no longer needed
 
--- Only run if events table exists
-DO $$
-BEGIN
-  IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'events') THEN
-    ALTER TABLE events DROP COLUMN IF EXISTS attendees;
-    ALTER TABLE events DROP COLUMN IF EXISTS email_attendees;
-    RAISE NOTICE 'Removed attendees columns from events table';
-  ELSE
-    RAISE NOTICE 'Events table does not exist - skipping migration';
-  END IF;
-END $$;
+ALTER TABLE events DROP COLUMN IF EXISTS attendees;
+ALTER TABLE events DROP COLUMN IF EXISTS email_attendees;
 
